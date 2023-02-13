a86607c7a05477ccd97c0df1c601a2210f93364c
rajendratechment <rajendra.verma@techment.com>
2023-02-13 18:21:10 +0530

    Error with Authentication provider
    {"code":400,"msg":"Unsupported provider: provider is not enabled"}
    https://supabase.com/docs/reference/python/auth-signinwithoauth

--- app.py
+++ app.py
@@ -3,5 +3,8 @@ from supabase import create_client, Client
 url = "https://mpriiaoimjjntvehtvsd.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wcmlpYW9pbWpqbnR2ZWh0dnNkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3NjI2Mzc5NywiZXhwIjoxOTkxODM5Nzk3fQ.JwD3VHCjsZwYSpQ1yDpD0-hTXeNiBef0vIzpMZrF_0E"
 supabase: Client = create_client(url, key)
-data = supabase.auth.sign_in_with_password({"email": "rajendrarajaramv@gmail.com", "password": "Zxasqw12!"})
+data = supabase.auth.sign_in_with_oauth({
+    "provider": 'github'
+})
+
 print(data)
