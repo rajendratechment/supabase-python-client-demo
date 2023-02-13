b470ff67e5407579f9ca84e30945021845b5f418
rajendratechment <rajendra.verma@techment.com>
2023-02-13 18:13:46 +0530

    I am getting error at this point can any one help me
    gotrue.errors.AuthRetryableError: 5 validation errors for User
    
    https://supabase.com/docs/reference/python/auth-signinwithotp

--- app.py
+++ app.py
@@ -3,5 +3,10 @@ from supabase import create_client, Client
 url = "https://mpriiaoimjjntvehtvsd.supabase.co"
 key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wcmlpYW9pbWpqbnR2ZWh0dnNkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3NjI2Mzc5NywiZXhwIjoxOTkxODM5Nzk3fQ.JwD3VHCjsZwYSpQ1yDpD0-hTXeNiBef0vIzpMZrF_0E"
 supabase: Client = create_client(url, key)
-data = supabase.auth.sign_in_with_password({"email": "rajendrarajaramv@gmail.com", "password": "Zxasqw12!"})
-print(data)
+
+data = supabase.auth.sign_in_with_otp({
+  "email": 'rajendrarajaramv@gmail.com',
+  "options": {
+    "email_redirect_to": 'https://example.com/welcome'
+  }
+})
