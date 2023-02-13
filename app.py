55057c659b82c2741ae52a6b7040bb92325888ee
rajendratechment <rajendra.verma@techment.com>
2023-02-13 20:15:40 +0530

    fixed the .env issue with junaid pip install python-dotenv
    >>> from dotenv import load_dotenv
    >>> load_dotenv()

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from supabase import create_client, Client
 
-url = "https://mpriiaoimjjntvehtvsd.supabase.co"
-key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wcmlpYW9pbWpqbnR2ZWh0dnNkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3NjI2Mzc5NywiZXhwIjoxOTkxODM5Nzk3fQ.JwD3VHCjsZwYSpQ1yDpD0-hTXeNiBef0vIzpMZrF_0E"
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+
+load_dotenv()
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_KEY")
 supabase: Client = create_client(url, key)
-data = supabase.auth.sign_in_with_password({"email": "rajendrarajaramv@gmail.com", "password": "Zxasqw12!"})
-print(data)
+r = supabase.from_('countries').select("*").execute()
+print(r)
