46d375aae788c09a96cce6072de15df935229958
rajendratechment <rajendra.verma@techment.com>
2023-02-13 10:40:36 +0530

    Python Client Library
    https://supabase.com/docs/reference/python/select
    
    1. initialization of the database
    2. create the table and insert some of values
    3. fetch the records
    .........................

---
+++ app.py
@@ -0,0 +1,8 @@
+from supabase import create_client, Client
+
+url = "https://mpriiaoimjjntvehtvsd.supabase.co"
+key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1wcmlpYW9pbWpqbnR2ZWh0dnNkIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY3NjI2Mzc5NywiZXhwIjoxOTkxODM5Nzk3fQ.JwD3VHCjsZwYSpQ1yDpD0-hTXeNiBef0vIzpMZrF_0E"
+supabase: Client = create_client(url, key)
+r = supabase
+response = r.from_('countries').select("*").execute()
+print(response)
